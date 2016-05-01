34ec86a733fe52b9a0cd42fec162e44f40e9dc00
Karen Ling <karensling@gmail.com>
2016-05-01 01:49:22 -0700

    Starting simple react app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Accounts::Application.config.secret_token = 'f1b6089a40f09dba44fc292194265bbfa32c48cedd9fa7cbf7e8cfd54f7acfaedf5a37d3296ec4ce3ab55750cbd15edc2eef4445fc1d065692b718a1e2685a6e'
